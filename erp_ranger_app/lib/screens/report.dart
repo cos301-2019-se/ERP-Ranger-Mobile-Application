b4b41ba6d54236c785acfda1404d47c64c25a8e3
u17080526 <47845621+u17080526@users.noreply.github.com>
2019-05-23 20:02:44 +0200

    Implementation of reports

--- erp_ranger_app/lib/screens/report.dart
+++ erp_ranger_app/lib/screens/report.dart
@@ -1,5 +1,7 @@
 import 'package:erp_ranger_app/components/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
 
 class ReportScreen extends StatefulWidget {
     @override
@@ -8,15 +10,29 @@ class ReportScreen extends StatefulWidget {
 
 class ReportState extends State<ReportScreen> {
   final TextEditingController _reportTextFieldController = new TextEditingController();
-  DateTime now;
+  final DatabaseReference _firebaseDatabase = FirebaseDatabase.instance.reference();
+  final Geolocator _geolocator = Geolocator();
 
-  void performReport() async {
+  Position _userLocation;
+  DateTime _now;
+  String _reportDetails;
+  String _reportType;
+
+  void _performReport() async {
+    _userLocation = await _geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    _now = new DateTime.now();
+    _firebaseDatabase.push().set({"location": _userLocation,
+      "park": "Rietvlei",
+      "report": _reportDetails,
+      "time": _now,
+      "type": _reportType,
+      "user": 1});
     _reportTextFieldController.clear();
-    now = new DateTime.now();
   }
 
   @override
   Widget build(BuildContext context) {
+    _firebaseDatabase.child("reports");
     return new Scaffold(
       appBar: new AppBar(
         title: Text("ERP Ranger Mobile App"),
@@ -29,6 +45,7 @@ class ReportState extends State<ReportScreen> {
         child: ListView(
           children: <Widget>[
             _showReportDetails(),
+            _showReportTypeList(),
             _showReportTextField(),
             _showReportButton()
           ],
@@ -38,7 +55,7 @@ class ReportState extends State<ReportScreen> {
   }
 
   Widget _showReportDetails() {
-    now = new DateTime.now();
+    _now = new DateTime.now();
     return RichText (
       text: new TextSpan(
         style: new TextStyle(
@@ -46,18 +63,36 @@ class ReportState extends State<ReportScreen> {
           color: Colors.blue
         ),
         children: <TextSpan>[
-          /*new TextSpan(text: 'Ranger name:\n'),
-          new TextSpan(text: 'Automatically Retrieved\n',style: new TextStyle(fontSize: 15.0,color: Colors.black)),
-          new TextSpan(text: 'Ranger ID:\n'),
-          new TextSpan(text: 'Automatically Retrieved\n',style: new TextStyle(fontSize: 15.0,color: Colors.black)),*/
-          new TextSpan(text: 'Location:\n'),
-          new TextSpan(text: 'Automatically Retrieved\n',style: new TextStyle(fontSize: 15.0,color: Colors.black)),
+          new TextSpan(text: 'Park:\n'),
+          new TextSpan(text: 'Rietvlei\n',style: new TextStyle(fontSize: 15.0,color: Colors.black)),
           new TextSpan(text: 'Date:\n'),
-          new TextSpan(text: now.day.toString()+'-'+now.month.toString()+'-'+now.year.toString()+'\n',style: new TextStyle(fontSize: 15.0,color: Colors.black)),
+          new TextSpan(text: _now.day.toString()+'-'+_now.month.toString()+'-'+_now.year.toString()+'\n',style: new TextStyle(fontSize: 15.0,color: Colors.black)),
           new TextSpan(text: 'Time:\n'),
-          new TextSpan(text: now.hour.toString()+':'+now.minute.toString(),style: new TextStyle(fontSize: 15.0,color: Colors.black)),
+          new TextSpan(text: _now.hour.toString()+':'+_now.minute.toString(),style: new TextStyle(fontSize: 15.0,color: Colors.black)),
         ]
       )
+    );
+  }
+
+  Widget _showReportTypeList() {
+    return Container(
+        padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+        child:DropdownButton<String>(
+          value: _reportType,
+          onChanged: (String value) {
+            setState(() {
+              _reportType = value;
+            });
+          },
+          items: <String>['Damage to property', 'Harmed animal', 'Intruder', 'Other']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          })
+              .toList(),
+        ),
     );
   }
 
@@ -77,32 +112,33 @@ class ReportState extends State<ReportScreen> {
           labelStyle: TextStyle(
             fontSize: 25.0
           )
-        )
+        ),
+        onChanged: (value) => this._reportDetails = value,
       )
     );
   }
 
   Widget _showReportButton() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
-      child: SizedBox(
-        height: 40.0,
-        child: new RaisedButton(
-          elevation: 5.0,
-          color: Colors.red,
-          shape: new RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(30.0)
-          ),
-          child: Text(
-            'Report',
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.white
-            )
-          ),
-          onPressed: performReport
-        )
-      )
-    );
-  }
+      return Padding(
+          padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+          child: SizedBox(
+              height: 40.0,
+              child: new RaisedButton(
+                  elevation: 5.0,
+                  color: Colors.blue,
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0)
+                  ),
+                  child: Text(
+                      'Report',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white
+                      )
+                  ),
+                  onPressed: _performReport
+              )
+          )
+      );
+    }
 }
