b9408ef9d857063a3809ffa9d203911996588f29
Eric Kirsten <ericmatthew.kirsten@gmail.com>
2019-06-22 09:43:34 +0200

    Update report detail component

--- erp-ranger-admin-portal/src/app/modules/admin/pages/report-detail/report-detail.component.ts
+++ erp-ranger-admin-portal/src/app/modules/admin/pages/report-detail/report-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from '../../services/report.service';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-report-detail',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportDetailComponent implements OnInit {
 
-  constructor() { }
+  report;
+
+  constructor(private reports: ReportService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.displayReports();
+  }
+
+  displayReports() {
+    this.reports.readReport(this.route.snapshot.paramMap.get("id")).subscribe(result => {
+      this.report = result;
+      console.log(this.report);
+    });
   }
 
 }
