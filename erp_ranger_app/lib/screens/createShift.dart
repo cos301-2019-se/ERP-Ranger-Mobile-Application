30ab3171ef3bdc6a500d4c3535544d27f556fe8d
P.C. de Villiers <p.c.devilliers@gmail.com>
2019-07-16 08:41:33 +0200

    screen-shifts
    Implemented dynamic retrieval of user ID and implemented cancel button to allow the user to reselect faulty times. Also linearised the date & time selection.

--- erp_ranger_app/lib/screens/createShift.dart
+++ erp_ranger_app/lib/screens/createShift.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:erp_ranger_app/services/auth.dart';
 
 class CreateShift extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => new ShiftState();
 }
@@ -18,42 +18,43 @@ class ShiftState extends State<CreateShift> {
   bool startFlag = false;
   bool endFlag = false;
   bool dateFlag = false;
+  bool midnightFlag = false;
 
   static Firestore db = Firestore.instance;
   static CollectionReference shiftRef = db.collection('shifts');
   DocumentReference parkRef = shiftRef.document('c0PvLdUAgLX9wtkoA4Ca');
 
-  String user = "gQvk9DfM0KSrdSONKfXtnLJ6e0P2";
+  Auth tempAuth = new Auth();
+
+  Future<String> user;
   String park = "iwGnWNuDC3m1hRzNNBT5";
 
-  Future<Null> _selectedDate(BuildContext context) async{
+  Future<Null> _selectedDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
         context: context,
         initialDate: selectedDate,
-        firstDate: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day),
+        firstDate: DateTime(
+            DateTime.now().year, DateTime.now().month, DateTime.now().day),
         lastDate: DateTime(2101));
     if (picked != null) {
       dateFlag = true;
       setState(() {
         selectedDate = picked;
       });
-    } else{
+    } else {
       dateFlag = false;
-      setState(() {
-
-      });
+      setState(() {});
     }
   }
 
-  void _selectStartDate(BuildContext context){
+  void _selectStartDate(BuildContext context) {
     dateFlag = true;
     _selectedDate(context);
   }
 
-  Future<Null> _selectedTime(BuildContext context) async{
-    final TimeOfDay picked = await showTimePicker(
-        context: context,
-        initialTime: TimeOfDay.now());
+  Future<Null> _selectedTime(BuildContext context) async {
+    final TimeOfDay picked =
+        await showTimePicker(context: context, initialTime: TimeOfDay.now());
     if (picked != null) {
       setState(() {
         selectedTime = picked;
@@ -65,112 +66,85 @@ class ShiftState extends State<CreateShift> {
           _setStart();
         } else if (selectedEndDateTime == null && startFlag && endFlag) {
           _setEnd();
-        }
-        else {
-        }
+        } else {}
         if (_validateTime()) {
-
-        }
-        else {
+        } else {
           print("error: please reselect times");
-          _resetPickers(context);
+          _updatePickers(context);
           startFlag = false;
           endFlag = false;
           selectedStartDateTime = null;
           selectedEndDateTime = null;
         }
       });
-    } else{
-      if(startFlag && !endFlag){
+    } else {
+      if (startFlag && !endFlag) {
         startFlag = false;
-      } else if(endFlag && !startFlag){
+      } else if (endFlag && !startFlag) {
         endFlag = false;
-      } else{
+      } else {
         startFlag = false;
         endFlag = false;
       }
-      setState(() {
-
-      });
+      setState(() {});
     }
   }
 
-  void _resetPickers(BuildContext context){
-    setState(() {
-
-    });
+  void _updatePickers(BuildContext context) {
+    setState(() {});
   }
 
-  bool _validateTime(){
-    if(dateFlag){
-      if(selectedDate.year == DateTime.now().year){
-        if(selectedDate.month == DateTime.now().month){
-          if(selectedDate.day == DateTime.now().day){
-            if(startFlag) {
-              if (selectedStartDateTime.hour < DateTime.now().hour) {
-                return false;
-              } else if (selectedStartDateTime.hour == DateTime.now().hour && selectedStartDateTime.minute < DateTime.now().minute - 5) {
-                return false;
-              } else if(endFlag){
-                if(selectedEndDateTime.hour < selectedStartDateTime.hour){
-                  return false;
-                } else if(selectedEndDateTime.hour == selectedStartDateTime.hour && selectedEndDateTime.minute < selectedStartDateTime.minute){
-                  return false;
-                }
-              } else{
-                return true;
-              }
-            }
-            if(endFlag && !startFlag){
-              if(selectedEndDateTime.hour < DateTime.now().hour){
-                return false;
-              } else if(selectedEndDateTime.hour == DateTime.now().hour && selectedStartDateTime.minute < DateTime.now().minute -5){
-                return false;
-              } else{
-                return true;
-              }
-            }
-          } else{
-            return true;
-          }
-        } else{
-          return true;
-        }
-      } else{
-        return true;
-      }
-    } else {
-      if(startFlag) {
+  void _resetPickers(BuildContext context){
+    selectedStartDateTime = null;
+    selectedEndDateTime = null;
+    dateFlag = false;
+    startFlag = false;
+    endFlag = false;
+    midnightFlag = false;
+    _updatePickers(context);
+  }
+
+  bool _validateTime() {
+    if (dateFlag) {
+      if (selectedDate.year == DateTime.now().year &&
+          selectedDate.month == DateTime.now().month &&
+          selectedDate.day == DateTime.now().day) {
         if (selectedStartDateTime.hour < DateTime.now().hour) {
           return false;
-        } else if (selectedStartDateTime.hour == DateTime.now().hour && selectedStartDateTime.minute < DateTime.now().minute - 5) {
+        } else if (selectedStartDateTime.hour == DateTime.now().hour &&
+            selectedStartDateTime.minute < DateTime.now().minute - 5) {
           return false;
-        } else if(endFlag){
-          if(selectedEndDateTime.hour < selectedStartDateTime.hour){
-            if(selectedStartDateTime.hour - selectedEndDateTime.hour <= MAX_SHIFT_LENGTH){
-              return true;
-            } else {
-              return false;
-            }
-          } else if(selectedEndDateTime.hour == selectedStartDateTime.hour && selectedEndDateTime.minute < selectedStartDateTime.minute){
-            return false;
-          }
-        } else{
+        } else if (selectedEndDateTime.hour < selectedStartDateTime.hour &&
+            (((24 - selectedStartDateTime.hour) + selectedEndDateTime.hour) <=
+                MAX_SHIFT_LENGTH)) {
+          midnightFlag = true;
+          return true;
+        } else if (selectedEndDateTime.hour == selectedStartDateTime.hour &&
+            selectedEndDateTime.minute < selectedStartDateTime.minute) {
+          return false;
+        } else if (selectedEndDateTime.hour < DateTime.now().hour) {
+          return false;
+        } else if (selectedEndDateTime.hour == DateTime.now().hour &&
+            selectedEndDateTime.minute < DateTime.now().minute - 5) {
+          return false;
+        } else {
+          return true;
+        }
+      } else {
+        if (selectedStartDateTime.hour > selectedEndDateTime.hour &&
+            (((24 - selectedStartDateTime.hour) + selectedEndDateTime.hour) <=
+                MAX_SHIFT_LENGTH)) {
+          midnightFlag = true;
+          return true;
+        } else if (selectedStartDateTime.hour == selectedEndDateTime.hour &&
+            selectedStartDateTime.minute < selectedEndDateTime.minute) {
+          return false;
+        } else {
           return true;
         }
       }
-      if(endFlag && !startFlag){
-        if(selectedEndDateTime.hour < DateTime.now().hour){
-          return false;
-        } else if(selectedEndDateTime.hour == DateTime.now().hour && selectedStartDateTime.minute < DateTime.now().minute -5){
-          return false;
-        } else{
-          return true;
-        }
-      }
-      else{
-        return true;
-      }
+    } else {
+      return false;
     }
   }
 
@@ -179,8 +153,9 @@ class ShiftState extends State<CreateShift> {
     _selectedTime(context);
   }
 
-  void _setStart(){
-      selectedStartDateTime = new DateTime(selectedDate.year, selectedDate.month, selectedDate.day, selectedTime.hour, selectedTime.minute);
+  void _setStart() {
+    selectedStartDateTime = new DateTime(selectedDate.year, selectedDate.month,
+        selectedDate.day, selectedTime.hour, selectedTime.minute);
   }
 
   void _selectEndDateTime(BuildContext context) {
@@ -188,37 +163,51 @@ class ShiftState extends State<CreateShift> {
     _selectedTime(context);
   }
 
-  void _setEnd(){
-      selectedEndDateTime = new DateTime(selectedDate.year, selectedDate.month, selectedDate.day, selectedTime.hour, selectedTime.minute);
+  void _setEnd() {
+    selectedEndDateTime = new DateTime(selectedDate.year, selectedDate.month,
+        selectedDate.day, selectedTime.hour, selectedTime.minute);
   }
 
-  Future<void> compileData() async{
-    Timestamp end = new Timestamp.fromDate(selectedEndDateTime);
+  Future<void> compileData() async {
+    user = tempAuth.getUserUid();
+    Timestamp end;
+    if (midnightFlag) {
+      selectedEndDateTime = new DateTime(
+          selectedEndDateTime.year,
+          selectedEndDateTime.month,
+          selectedEndDateTime.day + 1,
+          selectedEndDateTime.hour,
+          selectedEndDateTime.minute,
+          selectedEndDateTime.second);
+      end = new Timestamp.fromDate(selectedEndDateTime);
+    } else {
+      end = new Timestamp.fromDate(selectedEndDateTime);
+    }
     Timestamp start = new Timestamp.fromDate(selectedStartDateTime);
     Firestore.instance
         .collection('shifts')
         .add({
-      "end": end,
-      "park": park,
-      "start": start,
-      "user": user,
-    })
+          "end": end,
+          "park": park,
+          "start": start,
+          "user": user,
+        })
         .then((result) => {
-      Navigator.pop(context),
-    })
+              Navigator.pop(context),
+            })
         .catchError((err) => print(err));
   }
 
-  String displayDateTime(DateTime t){
-    if(t != null) {
+  String displayDateTime(DateTime t) {
+    if (t != null) {
       if (t.minute == 0) {
         return t.hour.toString() + ":00";
-      } else if(t.minute < 10){
+      } else if (t.minute < 10) {
         return t.hour.toString() + ":0" + t.minute.toString();
       } else {
         return t.hour.toString() + ":" + t.minute.toString();
       }
-    }else{
+    } else {
       return "";
     }
   }
@@ -239,14 +228,15 @@ class ShiftState extends State<CreateShift> {
             _showStartTimePicker(),
             _showEndTimePicker(),
             _showSendData(),
+            _showRepickTimes(),
           ],
         ),
       ),
     );
   }
-  
-  Widget _showDatePicker(){
-    if(!dateFlag) {
+
+  Widget _showDatePicker() {
+    if (!dateFlag) {
       return Padding(
         padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
         child: new TextField(
@@ -261,12 +251,11 @@ class ShiftState extends State<CreateShift> {
               prefixIcon: new Icon(
                 Icons.calendar_today,
                 color: Colors.grey,
-              )
-          ),
+              )),
           onTap: () => _selectStartDate(context),
         ),
       );
-    }else{
+    } else {
       return Padding(
         padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
         child: new TextField(
@@ -276,21 +265,24 @@ class ShiftState extends State<CreateShift> {
           maxLength: 1,
           maxLengthEnforced: true,
           decoration: new InputDecoration(
-              labelText: selectedDate.day.toString() + "/" + selectedDate.month.toString() + "/" + selectedDate.year.toString(),
+              labelText: selectedDate.day.toString() +
+                  "/" +
+                  selectedDate.month.toString() +
+                  "/" +
+                  selectedDate.year.toString(),
               border: OutlineInputBorder(),
               prefixIcon: new Icon(
                 Icons.calendar_today,
                 color: Colors.grey,
-              )
-          ),
+              )),
           onTap: () => _selectStartDate(context),
         ),
       );
     }
   }
 
-  Widget _showStartTimePicker(){
-    if(!startFlag) {
+  Widget _showStartTimePicker() {
+    if (dateFlag && !startFlag) {
       return Padding(
         padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
         child: new TextField(
@@ -305,8 +297,7 @@ class ShiftState extends State<CreateShift> {
               prefixIcon: new Icon(
                 Icons.watch,
                 color: Colors.grey,
-              )
-          ),
+              )),
           onTap: () => _selectStartDateTime(context),
         ),
       );
@@ -325,8 +316,7 @@ class ShiftState extends State<CreateShift> {
               prefixIcon: new Icon(
                 Icons.watch,
                 color: Colors.grey,
-              )
-          ),
+              )),
           onTap: () => _selectStartDateTime(context),
         ),
       );
@@ -334,7 +324,7 @@ class ShiftState extends State<CreateShift> {
   }
 
   Widget _showEndTimePicker() {
-    if (!endFlag) {
+    if (startFlag && !endFlag) {
       return Padding(
         padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
         child: new TextField(
@@ -349,12 +339,11 @@ class ShiftState extends State<CreateShift> {
               prefixIcon: new Icon(
                 Icons.watch,
                 color: Colors.grey,
-              )
-          ),
+              )),
           onTap: () => _selectEndDateTime(context),
         ),
       );
-    }else{
+    } else {
       return Padding(
         padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
         child: new TextField(
@@ -369,34 +358,43 @@ class ShiftState extends State<CreateShift> {
               prefixIcon: new Icon(
                 Icons.watch,
                 color: Colors.grey,
-              )
-          ),
+              )),
           onTap: () => _selectEndDateTime(context),
         ),
       );
     }
   }
-  
-  Widget _showSendData(){
+
+  Widget _showSendData() {
     return Padding(
-      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+      padding: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 20.0),
       child: SizedBox(
         height: 40.0,
         child: new RaisedButton(
             elevation: 5.0,
             shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(30.0)
-            ),
-            color:  Color.fromRGBO(18, 27, 65, 1.0),
-            child:  Text(
-                'Send Data',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white
-                )
-            ),
-            onPressed: () => compileData()
-        ),
+                borderRadius: new BorderRadius.circular(30.0)),
+            color: Color.fromRGBO(18, 27, 65, 1.0),
+            child: Text('Book Shift',
+                style: TextStyle(fontSize: 20.0, color: Colors.white)),
+            onPressed: () => compileData()),
+      ),
+    );
+  }
+
+  Widget _showRepickTimes() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+      child: SizedBox(
+        height: 40.0,
+        child: new RaisedButton(
+            elevation: 5.0,
+            shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(30.0)),
+            color: Color.fromRGBO(200, 0, 0, 1.0),
+            child: Text('Cancel',
+                style: TextStyle(fontSize: 20.0, color: Colors.white)),
+            onPressed: () => _resetPickers(context)),
       ),
     );
   }
