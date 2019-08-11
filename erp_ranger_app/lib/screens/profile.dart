8ddaf8c4db123c40d33e54c0259ec2b1a588aa4c
P.C. de Villiers <p.c.devilliers@gmail.com>
2019-08-11 12:43:01 +0200

    screen-profile
    -Made initial profile screen

---
+++ erp_ranger_app/lib/screens/profile.dart
@@ -0,0 +1,36 @@
+import 'package:erp_ranger_app/components/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:erp_ranger_app/services/auth.dart';
+import 'package:erp_ranger_app/services/park.dart';
+import 'package:erp_ranger_app/screens/dashboard.dart';
+
+class ProfileScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => ProfileState();
+}
+
+class ProfileState extends State<ProfileScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text("ERP Ranger Mobile App"),
+        elevation: .1,
+        backgroundColor: Color.fromRGBO(18, 27, 65, 1.0),
+      ),
+      drawer: new CustomDrawer(),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+        child: ListView(
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
