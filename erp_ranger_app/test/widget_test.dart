c22102a9f11a44f0db3c4ef7aac4be18cfe484c7
u17080526 <47845621+u17080526@users.noreply.github.com>
2019-08-21 00:00:55 +0200

    Update widget_test.dart

--- erp_ranger_app/test/widget_test.dart
+++ erp_ranger_app/test/widget_test.dart
@@ -8,6 +8,7 @@ import 'widget/login_test.dart' as login_test;
 import 'widget/dashboard_test.dart' as dashboard_test;
 import 'widget/report_test.dart' as report_test;
 import 'widget/shifts_test.dart' as shifts_test;
+import 'widget/ranger_test.dart' as ranger_test;
 
 void main() {
 
@@ -19,6 +20,7 @@ void main() {
     //asset_test.main();
     //assets_test.main();
     end_shift_test.main();
+    ranger_test.main();
   });
 
 }
