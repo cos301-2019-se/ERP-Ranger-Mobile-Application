57382341bf63abc7084dda6dd08b355ab038701e
DylanSchwikkard <dwschwikkard@gmail.com>
2019-10-10 21:58:30 +0200

    Report Filter

--- erp-ranger-admin-portal/src/app/modules/admin/pages/report-overview/report-overview.component.ts
+++ erp-ranger-admin-portal/src/app/modules/admin/pages/report-overview/report-overview.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 import { ReportService } from '../../services/report.service';
 import { Park } from 'src/app/models/Park.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { ShiftService } from '../../services/shift.service';
+import { MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-report-overview',
@@ -20,12 +23,20 @@ export class ReportOverviewComponent implements OnInit {
   // tslint:disable-next-line: max-line-length
   kml = 'https://gist.githubusercontent.com/Jtfnel/77b53014741ec9fce2ffc68d210cdf56/raw/cd8d5bbf2476c48512cb6d44694a52289aa52999/rietvlei.kml';
   defaultui;
-
+  reportArr :  reportRow[] = [];
   markers;
+  dataSource = new MatTableDataSource(this.reportArr);
+  displayedColumns: string[] = ['type', 'park','time','ranger'];
 
-  constructor(private reports: ReportService) { }
+  @ViewChild(MatPaginator) paginator:MatPaginator;
+  @ViewChild(MatSort) sort:MatSort;
+
+  constructor(private reports: ReportService,private shifts:ShiftService) {}
 
   ngOnInit() {
+    
+    
+    
     this.park = {
       id: '80a612fd-1e2f-4511-8a4a-2d9a87ae4ca5',
       created: 1577905200000,
@@ -72,11 +83,61 @@ export class ReportOverviewComponent implements OnInit {
     this.displayReports();
   }
 
+  
+
   // Retrieve all reports submitted to the database and add all markers to the map
   displayReports() {
-    this.reports.getReports().subscribe(result => {
+    this.reports.getReports().subscribe(result => {      
       this.markers = result;
+      this.reportArr = [];
+      for (let i = 0; i < result.length; i++) {
+        var parkID = String(result[i].payload.doc.data()['park']);
+        var userID = String(result[i].payload.doc.data()['user']);
+        let docRef = this.shifts.getUserName(userID);        
+        let getUser = docRef.get()
+        .then(userDoc => {
+          if(!result){
+            console.log("User not found ");
+            
+          } else{     
+            var userName = userDoc.data().name;
+            let docRef = this.shifts.getParkName(parkID);        
+            let getPark = docRef.get()
+            .then(parkDoc => {
+              if(!parkDoc.exists){
+                console.log("Park not found ");
+                
+              } else{    
+                var d = new Date(result[i].payload.doc.data()['time'].toDate());
+                var da = (d+"").substring(0,(d+"").indexOf("GMT")-1);
+                var parkName = parkDoc.data().name;
+                this.reportArr[i] = {
+                  id: String(result[i].payload.doc.id),
+                  park: parkName,
+                  time: da,
+                  ranger: userName,
+                  type: String(result[i].payload.doc.data()['type']),
+                  reportInfo : String(result[i].payload.doc.data()['report'])
+                };
+                
+                this.dataSource = new MatTableDataSource(this.reportArr);
+                this.dataSource.sort = this.sort;
+                this.dataSource.paginator = this.paginator;
+              }
+            })
+            .catch(err => {
+              console.log("Error getting document");         
+            });
+          }
+        })
+        .catch(err => {
+          console.log("Error getting document");         
+        });
+
+        
+      }      
     });
+    
   }
 
   // Set size of AGM map
@@ -84,4 +145,21 @@ export class ReportOverviewComponent implements OnInit {
     document.getElementById("map-agm").style.height = (document.body.offsetHeight-32) + "px";
   }
 
+  //Applies filter to table
+  search(filterVal: string){
+    this.dataSource=this.dataSource;
+    this.dataSource.filter = filterVal.trim().toLowerCase();
+    if(this.dataSource.paginator){
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+}
+export interface reportRow{
+  id,
+  park: string,
+  time:string,
+  ranger:string,
+  type:string,
+  reportInfo:string
 }
